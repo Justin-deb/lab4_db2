f038ee32e9d4de6586431306e435d89410bc39b1
Justin-deb <justinestradacruz2006@gmail.com>
2026-05-25 08:26:55 -0600

    init front and backend

---
+++ backend/src/test/java/com/ucr/videogame_store/VideogameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucr.videogame_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
