cd25df7d299becb515a8f33ddef0865d557e54b9
Justin-deb <justinestradacruz2006@gmail.com>
2026-05-26 11:13:49 -0600

    <feat> Added client module/controller, fixed viddeogameController and added .gitignore for frontend

--- backend/src/main/java/com/ucr/videogame_store/controller/client_module/ClientController.java
+++ backend/src/main/java/com/ucr/videogame_store/controller/client_module/ClientController.java
@@ -1,35 +1,34 @@
-package com.ucr.videogame_store.controller.videogame_module;
+package com.ucr.videogame_store.controller.client_module;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ucr.videogame_store.model.Videogame;
-import com.ucr.videogame_store.service.VideogameService;
-
+import com.ucr.videogame_store.model.Client;
+import com.ucr.videogame_store.service.ClientService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 @RestController
-@RequestMapping("/api/videogames")
-public class VideogameController {
+@RequestMapping("/api/client")
+public class ClientController {
     @Autowired
-    VideogameService videogameService;
+    ClientService clientService;
 
-    @GetMapping("/allVideogames")
-    public ResponseEntity<?> getAllVideogames() {
-        return ResponseEntity.status(HttpStatus.OK).body(videogameService.getAllVideogames());
+    @GetMapping("/allClients")
+    public ResponseEntity<?> getAllClients() {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.getAllClients());
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> createVideogame(@RequestBody Videogame videogame) {
+    public ResponseEntity<?> createClient(@RequestBody Client client) {
         try {
-            videogameService.createVideogame(videogame);
+            clientService.createClient(client);
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());
--- backend/src/main/java/com/ucr/videogame_store/service/ClientService.java
+++ backend/src/main/java/com/ucr/videogame_store/service/ClientService.java
@@ -1,12 +1,24 @@
 package com.ucr.videogame_store.service;
 
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ucr.videogame_store.model.Client;
 import com.ucr.videogame_store.repository.ClientRepository;
 
 @Service
 public class ClientService {
     @Autowired
     ClientRepository clientRepository;
+
+    public @Nullable Object getAllClients() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllClients'");
+    }
+
+    public void createClient(Client client) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createClient'");
+    }
 }
